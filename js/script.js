a4f25a84ebce7466356f4695c9ef15c5b87c70c9
eliott-colin <eliott.colin.pro@gmail.com>
2024-12-19 12:23:51 +0100

    Partie 2.2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 async function loadData() {
         const response = await fetch("https://hp-api.onrender.com/api/characters");
         const listHP = await response.json();
-
+        
         const first12Characters = listHP.slice(0, 12);
         console.log(first12Characters);
     
@@ -18,7 +18,114 @@ async function loadData() {
         characters.appendChild(newDiv)
     });
     
+    
+    
+
+
+
 }
+
+
+async function house() {
+    const response = await fetch("https://hp-api.onrender.com/api/characters");
+    const listHP = await response.json();
+
+    document.querySelector("#Gr").addEventListener("click", () => {
+        const result = listHP.filter(element => element.house === "Gryffindor");
+
+        const characters = document.querySelector(".characters");
+        characters.innerHTML = "";
+
+        const result12 = result.slice(0, 12);
+        result12.forEach(element => {
+            const newDiv = document.createElement("div");
+            const imageUrl = element.image || "images\characters\troll.jpg";
+            newDiv.innerHTML = `
+                <div>
+                    <a href="details.html">
+                      <img src="${element.image ? element.image : "./images/characters/troll.jpg"}" alt="${element.name}" class="${element.house}" />
+                    </a>
+                    <p>${element.name}</p>
+                </div>
+            `;
+            characters.appendChild(newDiv);
+        });
+    });
+
+    document.querySelector("#Hu").addEventListener("click", () => {
+        const result = listHP.filter(element => element.house === "Hufflepuff");
+
+        const characters = document.querySelector(".characters");
+        characters.innerHTML = "";
+
+        const result12 = result.slice(0, 12);
+        result12.forEach(element => {
+            const newDiv = document.createElement("div");
+            const imageUrl = element.image || "images\characters\troll.jpg";
+            newDiv.innerHTML = `
+                <div>
+                    <a href="details.html">
+                      <img src="${element.image ? element.image : "./images/characters/troll.jpg"}" alt="${element.name}" class="${element.house}" />
+                    </a>
+                    <p>${element.name}</p>
+                </div>
+            `;
+            characters.appendChild(newDiv);
+        });
+    });
+    document.querySelector("#Ra").addEventListener("click", () => {
+        const result = listHP.filter(element => element.house === "Ravenclaw");
+
+        const characters = document.querySelector(".characters");
+        characters.innerHTML = "";
+
+        const result12 = result.slice(0, 12);
+        result12.forEach(element => {
+            const newDiv = document.createElement("div");
+            const imageUrl = element.image || "images\characters\troll.jpg";
+            newDiv.innerHTML = `
+                <div>
+                    <a href="details.html">
+                      <img src="${element.image ? element.image : "./images/characters/troll.jpg"}" alt="${element.name}" class="${element.house}" />
+                    </a>
+                    <p>${element.name}</p>
+                </div>
+            `;
+            characters.appendChild(newDiv);
+        });
+    });
+    document.querySelector("#Sl").addEventListener("click", () => {
+        const result = listHP.filter(element => element.house === "Slytherin");
+
+        const characters = document.querySelector(".characters");
+        characters.innerHTML = "";
+
+        const result12 = result.slice(0, 12);
+        result12.forEach(element => {
+            const newDiv = document.createElement("div");
+            const imageUrl = element.image || "images\characters\troll.jpg";
+            newDiv.innerHTML = `
+                <div>
+                    <a href="details.html">
+                      <img src="${element.image ? element.image : "./images/characters/troll.jpg"}" alt="${element.name}" class="${element.house}" />
+                    </a>
+                    <p>${element.name}</p>
+                </div>
+            `;
+            characters.appendChild(newDiv);
+        });
+    });
+
+
+
+
+
+}
+
+house();
+
+
+
+
 loadData();
-
-
+house();
